cace9eee440c77be616e316d1ce73c27baf2fa5d
emi505inos <emi.inos505>
2023-06-12 11:37:57 -0300

    new file:   palindrome/palindrome.py

---
+++ palindrome/palindrome.py
@@ -0,0 +1,26 @@
+def convertInputsString():
+    rawInput = input("\nPlease enter a word, phrase, or sentence \nto check if it is a palindrome:  ")
+    rawString = rawInput.lower()
+    rawList = list(rawString)
+    return rawList
+def stripAnalphabetics(dirtyList):
+    analphabeticList = [" ", "-", ".", ",",":",";", "!", "?", "'","\""]
+    for character in analphabeticList:
+        if character in dirtyList:
+            dirtyList.remove(character)
+            return stripAnalphabetics(dirtyList)
+        return dirtyList
+def runPalindromeCheck(straightList):
+    reversedList = straightList[::-1]
+    if reversedList == straightList:
+        return 'The text you have entered is a palindrome!'
+    else:
+        return "The text you have entered is not a palindrome."
+    
+def main():
+    print("\nPalindrome checker")
+    originalList = convertInputsString()
+    originalList = stripAnalphabetics(originalList)
+    palindromeCheck = runPalindromeCheck(originalList)
+    print(palindromeCheck)
+main()
